b20e018ed1fe9b92f96cf539e43478187d698933
Alexander Egger <alexander.egger@gmail.com>
2016-06-27 09:59:39 +0200

    Synchronized using Any does not work.

--- lost-type-info-in-inheritance-hierarchy/src/main/kotlin/main.kt
+++ lost-type-info-in-inheritance-hierarchy/src/main/kotlin/main.kt
@@ -6,10 +6,44 @@ class Derived : Base(), Comparable<Derived> {
     }
 }
 
-fun main(args: Array<String>) {
+enum class Foo { bar1, bar2 }
+
+fun isDoesNotMatchTheBaseTypeIfDerivedTypeImplementsComparable() {
     val bla: Any = Derived()
     when {
-        bla is Base -> println("is Base")
-        else -> println("is not Base")
+        bla is Base -> println("OK   : is matches Base.")
+        else -> println("ERROR: val should be Base but is not.")
     }
 }
+
+
+fun illegalCastWhenComparingEnum() {
+    try {
+        val bla: Any = Foo.bar1
+        println("OK   : bla can be compared to Foo.bar1. Result: ${(bla as Comparable<Foo>).compareTo(Foo.bar1)}.")
+    } catch (ex: Throwable) {
+        println("ERROR: Exception when comparing bla to Foo.bar1 : ${ex.message}.")
+    }
+}
+
+fun synchronizedUsingAnyDoesNotWork() {
+    synchronized("sync") {
+        println("OK   : Synchronized on a String works.")
+    }
+    try {
+        synchronized(Any()) {
+            println("OK   : Synchronized on Any() works.")
+        }
+    } catch (ex: Throwable) {
+        println("ERROR: Exception using Any() in synchronized : ${ex.message}.")
+    }
+}
+
+
+fun main(args: Array<String>) {
+    isDoesNotMatchTheBaseTypeIfDerivedTypeImplementsComparable()
+    illegalCastWhenComparingEnum()
+    synchronizedUsingAnyDoesNotWork()
+}
+
+
